2852b9c976daaf5f258c7746cf74c09623ba450d
DevYuns <ydhun93@gmail.com>
2022-11-20 20:46:35 +0900

    refactor: refactor formfield to edittext

---
+++ app/components/edit-text.tsx
@@ -0,0 +1,77 @@
+import type {DetailedHTMLProps, InputHTMLAttributes} from 'react';
+
+import clsx from 'clsx';
+import {useEffect} from 'react';
+import {useState} from 'react';
+
+type ClassNames = {
+  label?: string;
+  input?: string;
+  error?: string;
+};
+
+type Props = {
+  id?: string;
+  htmlFor: string;
+  label: string;
+  className?: string;
+  classNames?: ClassNames;
+  type?: string;
+  value?: any;
+  error?: string;
+} & DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+
+export function EditText({
+  id,
+  htmlFor,
+  label,
+  type = 'text',
+  value,
+  onChange,
+  error,
+  className,
+  classNames,
+  onSubmit,
+  ...restProps
+}: Props) {
+  const [showError, setShowError] = useState(false);
+  useEffect(() => {
+    setShowError(true);
+  }, [error]);
+
+  return (
+    <div className={className}>
+      <label
+        htmlFor={htmlFor}
+        className={clsx('text-black dark:text-white', classNames?.label)}
+      >
+        {label}
+      </label>
+      <input
+        {...restProps}
+        onChange={(e) => {
+          setShowError(false);
+          onChange?.(e);
+        }}
+        type={type}
+        id={id}
+        name={htmlFor}
+        value={value}
+        className={clsx(
+          `
+        w-full p-3 rounded my-2 outline outline-1 focus:outline-2 outline-black
+      `,
+          classNames?.input,
+        )}
+      />
+      <div
+        className={clsx(
+          'text-xs tracking-wide text-red w-full',
+          classNames?.error,
+        )}
+      >
+        {showError ? error : null}
+      </div>
+    </div>
+  );
+}
